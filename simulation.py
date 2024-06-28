e17349a5af6c494bcabf35d269c3da9a69f80572
Luke Jennings <luke@lawn-128-61-54-30.lawn.gatech.edu>
2024-06-28 11:42:05 -0400

    reworked intersect detection logic

--- simulation.py
+++ simulation.py
@@ -44,41 +44,77 @@ class Node:
             cartx_other = other.pos[0] - SCREEN_WIDTH/2
             carty_other = SCREEN_HEIGHT/2 - other.pos[1]
 
-            for n in avoid:
-                m = (carty_self - carty_other) / (cartx_self - cartx_other) 
-                b = carty_self - cartx_self * m
+            vertical = True if self._pos[0] == other.pos[0]
 
-                cartx_avoid = n.pos[0] - SCREEN_WIDTH/2
-                carty_avoid = SCREEN_HEIGHT/2 - n.pos[1]
+            x_intersects = []
+            y_intersects = []
+            if not vertical:
+                for n in avoid:
+                    cartx_avoid = n.pos[0] - SCREEN_WIDTH/2
+                    carty_avoid = SCREEN_HEIGHT/2 - n.pos[1]
 
-                a = m**2+1
-                B = 2*(m*b-m*carty_avoid-cartx_avoid)
-                c = carty_avoid**2 - r**2 + cartx_avoid**2 - 2*b*carty_avoid + b**2
+                    x_intersects += find_intersections([cartx_self, carty_self], [cartx_other, carty_other], [cartx_avoid, carty_avoid], r)
                 
-                x1 = (-B+math.sqrt(B**2-4*a*c))/(2*a)
-                x2 = (-B-math.sqrt(B**2-4*a*c))/(2*a)
-
-                x1 += SCREEN_WIDTH/2
-                x2 += SCREEN_WIDTH/2
-                
-                m = (self._pos[1] - other.pos[1]) / (self._pos[0] - other.pos[0]) 
-                b = self._pos[1] - m*self._pos[0]
-
-
                 if self._pos[0] < other.pos[0]:
-                    self.draw_around(other, min(x1, x2), max(x1, x2), min(x1, x2)*m+b, max(x1, x2)*m+b, n, r)
-                elif self._pos[0] > other.pos[0]:
-                    self.draw_around(other, max(x1, x2), min(x1, x2), max(x1, x2)*m+b, min(x1, x2)*m+b, n, r)
+                    #left to right
+                    x_intersects.sort()
                 else:
-                    if self._pos[1] > other.pos[1]:
-                        self.draw_around(other, (max(y1, y2)-b)/m, (min(y1, y2)-b)/m, max(y1, y2), min(y1, y2), n, r)
-                    elif self._pos[1] < other.pos[1]:
-                        self.draw_around(other, (min(y1, y2)-b)/m, (max(y1, y2)-b)/m, min(y1, y2), max(y1, y2), n, r)
+                    x_intersects.sort(reverse=True)
 
-    def draw_around(self, other: "Node", x1: float, x2: float, y1: float, y2: float, n: "Node", radius: int) -> None:
-        pygame.draw.line(surface, WHITE, self._pos, (x1, y1), 1)
+                m = (self._pos[1] - other.pos[1]) / (self._pos[0] - other.pos[0])     
+                b = self._pos[1] - m*self._pos[0]
+                y_intersects = [m*x+b for x in x_intersects]
+
+
+            else:
+                x_intersects = [n.pos[0] for n in avoid]
+                y_intersects += [n.pos[1]+r for n in avoid]
+                y_intersects += [n.pos[1]-r for n in avoid]
+
+                if self._pos[1] < other.pos[1]:
+                    #top to bottom
+                    y_intersects.sort()
+                else
+                    y_intersects.sort(reverse=True)
+            
+
+
+            
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+                #x1, x2 = find_intersections([cartx_self, carty_self], [cartx_other, carty_other], [cartx_avoid, carty_avoid], r) 
+                #if i == len(avoid)-1:
+                #    next_n = other
+                #else:
+                #    next_n = avoid[i+1]
+                #if self._pos[0] < other.pos[0]:
+                 #   prev.draw_around(n, next_n, min(x1, x2), max(x1, x2), min(x1, x2)*m+b, max(x1, x2)*m+b, r)
+                    #self.draw_around(other, min(x1, x2), max(x1, x2), min(x1, x2)*m+b, max(x1, x2)*m+b, n, r)
+                #elif self._pos[0] > other.pos[0]:
+                #    self.draw_around(other, max(x1, x2), min(x1, x2), max(x1, x2)*m+b, min(x1, x2)*m+b, n, r)
+                #else:
+                #    if self._pos[1] > other.pos[1]:
+                #        self.draw_around(other, (max(y1, y2)-b)/m, (min(y1, y2)-b)/m, max(y1, y2), min(y1, y2), n, r)
+                #    elif self._pos[1] < other.pos[1]:
+                #        self.draw_around(other, (min(y1, y2)-b)/m, (max(y1, y2)-b)/m, min(y1, y2), max(y1, y2), n, r)
+
+
+    def draw_around(self, n: "Node", next_n: "Node", x1: float, x2: float, y1: float, y2: float, radius: int) -> None: #self is prev
+        pygame.draw.line(surface, WHITE, self.pos, (x1, y1), 1)
         pygame.draw.circle(surface, WHITE, n.pos, radius, 1)
-        pygame.draw.line(surface, WHITE, (x2, y2), other.pos, 1)
+        pygame.draw.line(surface, WHITE, (x2, y2), next_n.pos, 1)
                     
     
     def find_obstructions(self, other: "Node", nodes: list["Node"]) -> list["Node"]:
@@ -153,6 +189,23 @@ class Node:
     @color.setter
     def color(self, newcolor: list[int]) -> None:
         self._color = newcolor
+
+def find_intersections(cart_self: list[float], cart_other: list[float], cart_avoid: list[float], r: int) -> list[float]: #assume m and b are cartesian
+    m = (cart_self[1] - cart_other[1]) / (cart_self[0] - cart_other[0]) 
+    b = cart_self[1] - cart_self[0] * m
+
+    a = m**2+1
+    B = 2*(m*b-m*cart_avoid[1]-cart_avoid[0])
+    c = cart_avoid[1]**2 - r**2 + cart_avoid[0]**2 - 2*b*cart_avoid[1] + b**2
+                
+    x1 = (-B+math.sqrt(B**2-4*a*c))/(2*a)
+    x2 = (-B-math.sqrt(B**2-4*a*c))/(2*a)
+
+    x1 += SCREEN_WIDTH/2
+    x2 += SCREEN_WIDTH/2
+
+    return [x1, x2]
+
 
 def calculate_distance(n1: list[int], n2: list[int]) -> float:
     return sum([(n1.pos[i]-n2.pos[i])**2 for i in range(len(n1.pos))])**0.5
