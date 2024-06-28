024d09f1a1bc64adb9c11d0e988ad082ddbd95e7
CalebBunch <rcbcg123@gmail.com>
2024-06-28 16:02:02 -0400

    done

--- simulation.py
+++ simulation.py
@@ -2,11 +2,13 @@ import pygame
 import sys
 import random
 import math
+import time
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
+RED = [255, 0, 0]
 MIN_SEPERATION = 20
 NUM_NODES = 54
 OFFSET = 50
@@ -20,7 +22,10 @@ class Node:
     def __init__(self, pos: list[int]) -> None:
         self._pos = pos
         self._connect_prob = random.uniform(0, 1)
-        self._meetup_prob = random.uniform(0, 1)
+        self._disconnect_prob = random.uniform(0, 1)
+        self._relationship_prob = random.uniform(0, 1)
+        self._death_prob = random.uniform(0, 1)
+        self._birth_prob = random.uniform(0, 1)
         self._neighbors = []
         self._friends = []
         self._gender = bool(random.getrandbits(1)) #1 is male, 0 is female
@@ -28,24 +33,22 @@ class Node:
         self._color = WHITE
 
     def connect(self, other: "Node", nodes: list["Node"]) -> None:
-        # This is fricked - need to stop drawing lines between self and other every iteration of for loop
-        self._friends.append(other)
+        connect_color = RED if self.partner == other else WHITE
+
         if other in self._neighbors:
-            pygame.draw.line(surface, WHITE, self._pos, other.pos, 1)
+            pygame.draw.line(surface, connect_color, self._pos, other.pos, 1)
         else:
             avoid = self.find_obstructions(other, nodes)
-            #assert len(avoid) > 0, "no obstructions"
             if len(avoid) == 0:
-                pygame.draw.line(surface, WHITE, self._pos, other.pos, 1)
+                pygame.draw.line(surface, connect_color, self._pos, other.pos, 1)
+                return
+                
             r = 15
             cartx_self = self._pos[0] - SCREEN_WIDTH/2
             carty_self = SCREEN_HEIGHT/2 - self._pos[1]
-            
             cartx_other = other.pos[0] - SCREEN_WIDTH/2
             carty_other = SCREEN_HEIGHT/2 - other.pos[1]
-
             vertical = True if self._pos[0] == other.pos[0] else False
-
             x_intersects = []
             y_intersects = []
             if not vertical:
@@ -54,7 +57,6 @@ class Node:
                     carty_avoid = SCREEN_HEIGHT/2 - n.pos[1]
 
                     x_intersects += find_intersections([cartx_self, carty_self], [cartx_other, carty_other], [cartx_avoid, carty_avoid], r)
-                
                 if self._pos[0] < other.pos[0]:
                     #left to right
                     x_intersects.sort()
@@ -65,9 +67,9 @@ class Node:
                 b = self._pos[1] - m*self._pos[0]
                 y_intersects = [m*x+b for x in x_intersects]
 
-
             else:
-                x_intersects = [n.pos[0] for n in avoid]
+                x_intersects = [avoid[0].pos[0] for _ in range(len(avoid)*2)]
+                
                 y_intersects += [n.pos[1]+r for n in avoid]
                 y_intersects += [n.pos[1]-r for n in avoid]
 
@@ -76,65 +78,60 @@ class Node:
                     y_intersects.sort()
                 else:
                     y_intersects.sort(reverse=True)
-            
 
+            self.draw_around(other, avoid, x_intersects, y_intersects, r, connect_color)
 
-            
-                
+    def draw_around(self, other: "Node", nodes: list["Node"], x_i: list[float], y_i: list[float], radius: int, color: list[int]) -> None:
+        pygame.draw.line(surface, color, self.pos, (x_i[0], y_i[0]), 1)
 
+        prev, curr = 1, 2
+        while curr < len(x_i):
+            pygame.draw.line(surface, color, (x_i[prev], y_i[prev]), (x_i[curr], y_i[curr]), 1)
+            prev += 2
+            curr += 2
 
+        pygame.draw.line(surface, color, (x_i[prev], y_i[prev]), other.pos, 1)
 
+        for n in nodes:
+            pygame.draw.circle(surface, color, n.pos, radius, 1)
 
-
-
-
-
-
-
-
-
-
-                #x1, x2 = find_intersections([cartx_self, carty_self], [cartx_other, carty_other], [cartx_avoid, carty_avoid], r) 
-                #if i == len(avoid)-1:
-                #    next_n = other
-                #else:
-                #    next_n = avoid[i+1]
-                #if self._pos[0] < other.pos[0]:
-                 #   prev.draw_around(n, next_n, min(x1, x2), max(x1, x2), min(x1, x2)*m+b, max(x1, x2)*m+b, r)
-                    #self.draw_around(other, min(x1, x2), max(x1, x2), min(x1, x2)*m+b, max(x1, x2)*m+b, n, r)
-                #elif self._pos[0] > other.pos[0]:
-                #    self.draw_around(other, max(x1, x2), min(x1, x2), max(x1, x2)*m+b, min(x1, x2)*m+b, n, r)
-                #else:
-                #    if self._pos[1] > other.pos[1]:
-                #        self.draw_around(other, (max(y1, y2)-b)/m, (min(y1, y2)-b)/m, max(y1, y2), min(y1, y2), n, r)
-                #    elif self._pos[1] < other.pos[1]:
-                #        self.draw_around(other, (min(y1, y2)-b)/m, (max(y1, y2)-b)/m, min(y1, y2), max(y1, y2), n, r)
-
-
-    def draw_around(self, n: "Node", next_n: "Node", x1: float, x2: float, y1: float, y2: float, radius: int) -> None: #self is prev
-        pygame.draw.line(surface, WHITE, self.pos, (x1, y1), 1)
-        pygame.draw.circle(surface, WHITE, n.pos, radius, 1)
-        pygame.draw.line(surface, WHITE, (x2, y2), next_n.pos, 1)
-                    
-    
     def find_obstructions(self, other: "Node", nodes: list["Node"]) -> list["Node"]:
         obstructors = []
-        m = (self._pos[1] - other.pos[1]) / (self._pos[0] - other._pos[0]) 
-        b = self._pos[1] - self._pos[0] * m
-        #pygame.draw.line(surface, WHITE, (self._pos[0],m*self._pos[0]+b), (other.pos[0],m*other.pos[0]+b),1)
-        for n in nodes:
-            if abs(n.pos[1] - (n.pos[0] * m + b)) < 15 and n != self and n != other:
-                if n.pos[0] > min(other.pos[0], self._pos[0]) and n.pos[0] < max(other.pos[0], self._pos[0]):
-                    obstructors.append(n)
+        if self._pos[0] == other.pos[0]:
+            for n in nodes:
+                if abs(n.pos[0] - other.pos[0]) < 15 and n != self and n != other:
+                    if n.pos[1] > min(other.pos[1], self._pos[1]) and n.pos[1] < max(other.pos[1], self._pos[1]):
+                        obstructors.append(n)
+        else:
+            m = (self._pos[1] - other.pos[1]) / (self._pos[0] - other._pos[0]) 
+            b = self._pos[1] - self._pos[0] * m
+            for n in nodes:
+                if abs(n.pos[1] - (n.pos[0] * m + b)) < 15 and n != self and n != other:
+                    if n.pos[0] > min(other.pos[0], self._pos[0]) and n.pos[0] < max(other.pos[0], self._pos[0]):
+                        obstructors.append(n)
 
         return obstructors
+    
+    def find_adjacent(self, nodes: list["Node"]) -> list["Node"]:
+        adj = []
+        for n in nodes:
+            if calculate_distance(self, n) < 150 and n != self:
+                adj.append(n)
+        return adj
 
+    def possible_friends(self, nodes: list["Node"]) -> list["Node"]:
+        poss_friends = []
+        for n in self._neighbors:
+            if n not in self._friends:
+                poss_friends.append(n)
+        for f in self._friends:
+            for _f in f.friends:
+                for __f in _f.friends:
+                    if __f != _f and __f != f and __f not in self._friends:
+                        poss_friends.append(__f)
 
-    """
-    pygame.draw.arc(surface, WHITE, (200, 200, 100, 20), 0, math.pi, 1)
-    pygame.draw.circle(surface, WHITE, (200, 215), 2)
-    pygame.draw.circle(surface, WHITE, (300, 215), 2)
-    """
+        return poss_friends
+    
     def draw_node(self) -> None:
         pygame.draw.circle(surface, self._color, self._pos, 5)
     
@@ -175,8 +172,12 @@ class Node:
         return self._connect_prob
 
     @property
-    def meetup_prob(self) -> float:
-        return self._meetup_prob
+    def disconnect_prob(self) -> float:
+        return self._disconnect_prob
+
+    @property
+    def relationship_prob(self) -> float:
+        return self._relationship_prob
 
     @property
     def gender(self) -> bool:
@@ -190,33 +191,70 @@ class Node:
     def color(self, newcolor: list[int]) -> None:
         self._color = newcolor
 
-def find_intersections(cart_self: list[float], cart_other: list[float], cart_avoid: list[float], r: int) -> list[float]: #assume m and b are cartesian
+    @property
+    def death_prob(self) -> float:
+        return self._death_prob
+    
+    @property
+    def birth_prob(self) -> float:
+        return self._birth_prob
+
+def find_intersections(cart_self: list[float], cart_other: list[float], cart_avoid: list[float], r: int) -> list[float]: 
     m = (cart_self[1] - cart_other[1]) / (cart_self[0] - cart_other[0]) 
     b = cart_self[1] - cart_self[0] * m
-
     a = m**2+1
     B = 2*(m*b-m*cart_avoid[1]-cart_avoid[0])
     c = cart_avoid[1]**2 - r**2 + cart_avoid[0]**2 - 2*b*cart_avoid[1] + b**2
-                
     x1 = (-B+math.sqrt(B**2-4*a*c))/(2*a)
     x2 = (-B-math.sqrt(B**2-4*a*c))/(2*a)
-
     x1 += SCREEN_WIDTH/2
     x2 += SCREEN_WIDTH/2
 
     return [x1, x2]
 
-
-def calculate_distance(n1: list[int], n2: list[int]) -> float:
+def calculate_distance(n1: Node, n2: Node) -> float:
     return sum([(n1.pos[i]-n2.pos[i])**2 for i in range(len(n1.pos))])**0.5
 
-def update() -> None:
-    pass
+def update(node: Node, nodes: list[Node], spaces: [list[list[float]]]) -> (list[Node], list[list[float]]):
+    rand_id = random.uniform(0, 1)
+    if node.death_prob < rand_id*0.2:
+        spaces.append(node.pos)
+        for n in nodes:
+            if node in n.friends:
+                n.friends.remove(node)
+            if node in n.neighbors:
+                n.neighbors.remove(node)
+            if node == n.partner:
+                n.partner = None
+            
+        nodes.remove(node)
+
+    if node.partner is not None and (node.birth_prob + node.partner.birth_prob)/2 < rand_id*0.4 and len(spaces) > 0:
+        nodes.append(Node(s:=random.choice(spaces)))
+        spaces.remove(s)
+        for n in nodes:
+            n.neighbors = n.find_adjacent(nodes)
+
+    poss_friends = node.possible_friends(nodes)
+    if node.connect_prob < rand_id and len(poss_friends) > 0:
+        node.friends.append(random.choice(poss_friends))
+
+    for f in node.friends:
+        node.connect(f, nodes)
+        if node.disconnect_prob < rand_id*0.8:
+            node.friends.remove(f)
+            node.partner = None
+            f.partner = None
+        elif (node.gender != f.gender) and (node.partner is None and f.partner is None) and (node.relationship_prob < rand_id):
+            node.partner = f
+            f.partner = node
+
+    return nodes, spaces
 
 def generate_nodes() -> list[Node]:
     nodes = []
     n = round(NUM_NODES**0.5)
-    space = math.floor((SCREEN_WIDTH-OFFSET)/n) # spacing for nodes
+    space = math.floor((SCREEN_WIDTH-OFFSET)/n)
     for i in range(n):
         for j in range(n):
             x, y = space * i + math.floor(OFFSET), space * j + math.floor(OFFSET)
@@ -224,15 +262,16 @@ def generate_nodes() -> list[Node]:
                 y += math.floor(OFFSET)
             
             nodes.append(Node([x, y]))
-            
-    return nodes
+    for n in nodes:
+        n.neighbors = n.find_adjacent(nodes)
 
+    return nodes
 
 def main() -> None:
     run = True
     clock = pygame.time.Clock()
     nodes = generate_nodes()
-
+    spaces = []
     while run:
         clock.tick(60)
         keys = pygame.key.get_pressed()
@@ -245,18 +284,12 @@ def main() -> None:
 
         surface.fill(BLACK)
         
-        # update()
-        i = 0
         for node in nodes:
+            nodes, spaces = update(node, nodes, spaces)
             node.draw_node()
-            if i == 2:
-                node._color = (255, 0, 0)
-                nodes[21]._color = (0, 255, 0)
-                node.connect(nodes[21], nodes)
-            
-            i += 1
 
         display.flip()
+        time.sleep(1)
 
     pygame.quit()
     sys.exit()
